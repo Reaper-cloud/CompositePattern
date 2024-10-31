045698a83be312d025f94dcacbb70c2cd1560cf0
PennerDY <PennerDY@310D-9>
2024-10-31 16:05:40 +0700

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+public interface IDocumentComponent
+{
+    void Add(IDocumentComponent component);
+    void Remove(IDocumentComponent component);
+    void Display(int indent, IList<string> output);
+}
+public class Document
+{
+    private List<IDocumentComponent> _sections = new List<IDocumentComponent>();
+
+    public void AddSection(IDocumentComponent section)
+    {
+        _sections.Add(section);
+    }
+
+    public void RemoveSection(IDocumentComponent section)
+    {
+        _sections.Remove(section);
+    }
+
+    public IList<string> Display()
+    {
+        List<string> output = new List<string>();
+        foreach (var section in _sections)
+        {
+            section.Display(0, output);
+        }
+        return output;
+    }
+}
+public class Paragraph : IDocumentComponent
+{
+    private string _text;
+
+    public Paragraph(string text)
+    {
+        _text = text;
+    }
+
+    public void Add(IDocumentComponent component)
+    {
+        throw new NotSupportedException("Cannot add to a paragraph.");
+    }
+
+    public void Remove(IDocumentComponent component)
+    {
+        throw new NotSupportedException("Cannot remove from a paragraph.");
+    }
+
+    public void Display(int indent, IList<string> output)
+    {
+        output.Add(new string(' ', indent) + _text);
+    }
+}
+public class Section : IDocumentComponent
+{
+    private string _title;
+    private List<IDocumentComponent> _components = new List<IDocumentComponent>();
+
+    public Section(string title)
+    {
+        _title = title;
+    }
+
+    public void Add(IDocumentComponent component)
+    {
+        _components.Add(component);
+    }
+
+    public void Remove(IDocumentComponent component)
+    {
+        _components.Remove(component);
+    }
+
+    public void Display(int indent, IList<string> output)
+    {
+        output.Add(new string(' ', indent) + _title);
+        foreach (var component in _components)
+        {
+            component.Display(indent + 2, output);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection.Metadata;
+
+namespace DocumentManagement
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDocument();
+        }
+
+        private void LoadDocument()
+        {
+            Document document = new Document();
+
+            Section section1 = new Section("Introduction");
+            section1.Add(new Paragraph("This is the introduction paragraph."));
+
+            Section section2 = new Section("Main Content");
+            section2.Add(new Paragraph("This is the first paragraph of the main content."));
+            section2.Add(new Paragraph("This is the second paragraph of the main content."));
+
+            Section subsection = new Section("Subsection");
+            subsection.Add(new Paragraph("This is a paragraph in the subsection."));
+
+            section2.Add(subsection);
+
+            document.AddSection(section1);
+            document.AddSection(section2);
+
+            // Отображение документа в ListBox
+            DocumentListBox.ItemsSource = document.Display();
+        }
+    }
+}
